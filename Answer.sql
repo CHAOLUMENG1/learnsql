a521accc439dbae4591e546a66152f9dbc4dc6bb
CHAOLUMENG1 <129593598+CHAOLUMENG1@users.noreply.github.com>
2023-04-18 19:01:34 +0900

    Add files via upload

--- Answer.sql
+++ Answer.sql
@@ -207,16 +207,12 @@ where
 order by
     examination_date desc 
 limit
-    3 offset 1;
-    
-    
+    3 offset 1; 
+
 --再練習--
-
-
 --Q1 患者名が「鈴木花子」である患者の、最も新しい診察記録の診察ID、診察日、医師名、診療科名を表示するSQL文を作成してください。
 select
-    s.examination_id
-    , s.examination_date
+    s.examination_id, s.examination_date
     , d.doctor_name
     , dp.department_name 
 from
@@ -422,6 +418,183 @@ where
 order by
     examination_date desc 
 limit
-    3 offset 1;
+    3 offset 1; 
 
+--21."骨"で始まるdiagnosisを持つ患者を選択するSQL文を書いてください。
+select
+    * 
+from
+    examinations 
+where
+    diagnosis like '骨%'; 
+
+--22."慢性中耳炎"という単語を含むdiagnosisを持つ患者を選択するSQL文を書いてください。
+select
+    * 
+from
+    examinations 
+where
+    diagnosis like '%慢性中耳炎%'; 
+
+--23.安静にして湿布を貼るように指示で終わるpatient_idを持つ患者を選択するSQL文を書いてください。
+select
+    * 
+from
+    examinations 
+where
+    treatment like '%貼るように指示'; 
+
+--24.patient_idが3、5、8、10の患者の全診断情報を取得するSQLクエリを作成してください。
+select
+    * 
+from
+    examinations 
+where
+    patient_id in (3, 5, 8, 10); 
+
+--25.患者IDが2, 4, 6, 8のいずれかである診察レコードを検索してください。ただし、診察科が '小児科' であるものに限定します。
+select
+    * 
+from
+    examinations 
+where
+    patient_id in (2, 4, 6, 8) 
+    and department_id = ( 
+        select
+            department_id 
+        from
+            departments 
+        where
+            department_name = '小児科'
+    ); 
+
+--26.診察日が2022年3月15日または5月20日であり、診察科が '外科' または '歯科' のいずれかである診察レコードを検索してください。ただし、患者IDが4であるものは除外します。
+select
+    * 
+from
+    examinations 
+where
+    ( 
+        examination_date = '2022/03/15' 
+        or examination_date = '2022/05/20'
+    ) 
+    and department_id in ( 
+        select
+            department_id 
+        from
+            departments 
+        where
+            department_name in ('外科', '歯科')
+    ) 
+    and patient_id not in (4); 
+
+--27.検査日が2022年3月から2022年5月の間にある患者のうち、病院の外来で診察された患者の検査IDと診断結果を取得する。
+select
+    examination_id, diagnosis 
+from
+    examinations 
+where
+    examination_date between '2022-03-15' and '2022-05-20'; 
+
+--28.検査日が2022年3月から5月の間にある患者で、診断結果に「胃潰瘍」という文字列が含まれている患者の検査ID、患者ID、医師ID、診断結果を取得する。
+select
+    examination_id
+    , patient_id
+    , doctor_id
+    , diagnosis 
+from
+    examinations 
+where
+    examination_date between '2022-03-01' and '2022-05-31' 
+    and diagnosis like '胃潰%'; 
+
+--29.患者名、性別、診断、治療、および医師名を含む、すべての患者の詳細とその医師の情報を取得するSQL文を作成してください。
+select
+    p.patient_name
+    , p.gender
+    , e.diagnosis
+    , e.treatment
+    , d.doctor_name 
+from
+    patients p 
+    inner join examinations e 
+        on p.patient_id = e.patient_id 
+    inner join doctors d 
+        on e.doctor_id = d.doctor_id; 
+
+--30.各診療科の診断の数を含む、診療科名と診断数を表示するSQL文を作成してください。
+select
+    d.department_name
+    , count(*) as diagnosis_count 
+from
+    examinations e 
+    inner join departments d 
+        on e.department_id = d.department_id 
+group by
+    d.department_name; 
+
+--31.医師の情報と診断名を結合し、診断名が "慢性中耳炎" である患者の氏名と診断日を表示するクエリ。
+select
+    p.patient_name
+    , e.examination_date 
+from
+    patients as p 
+    inner join examinations e 
+        on p.patient_id = e.patient_id 
+    inner join doctors d 
+        on e.doctor_id = d.doctor_id 
+where
+    d.department_id = 1 
+    and e.diagnosis = '慢性中耳炎'; 
+
+--32.特定の科に所属する医師と、その医師が診断した患者の氏名と診断名を表示するクエリ。
+select
+    doctor_name
+    , p.patient_name
+    , e.diagnosis 
+from
+    patients p 
+    inner join examinations e 
+        on p.patient_id = e.patient_id 
+    inner join doctors d 
+        on e.doctor_id = d.doctor_id 
+    inner join departments dep 
+        on d.department_id = dep.department_id 
+where
+    dep.department_name = '外科'; 
+
+--33.患者ごとに、彼らが最後に受けた検査の情報と、その検査を行った医師の情報を表示する。
+--※（もし患者がまだ検査を受けていない場合は、NULL値が表示してください。）"
+select
+    p.patient_name
+    , e.examination_date
+    , d.doctor_name
+    , de.department_name 
+from
+    patients p 
+    left join ( 
+        select
+            examination_id
+            , patient_id
+            , doctor_id
+            , department_id
+            , examination_date 
+        from
+            examinations e1 
+        where
+            not exiete( 
+                select
+                    1 
+                from
+                    examinations e2 
+                where
+                    e1.patient_id = e2.patient_id 
+                    and e1.examination_date < e2.examination_date
+            )
+    ) e 
+        on p.patient_id = e.patient_id 
+    left join doctors d 
+        on e.doctor_id = d.doctor_id 
+    left join departments de 
+        on e.department_id = de.department_id;
 
